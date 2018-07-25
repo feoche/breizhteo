46b0127deb0e94961f53e4b9d96292740ddd5580
feoche <francois.eoche@corp.ovh.com>
2018-07-26 00:47:38 +0200

    Initial commit

---
+++ bot.js
@@ -0,0 +1,55 @@
+import Twitter from 'twitter';
+import minimist from 'minimist';
+import request from 'request';
+import moment from 'moment';
+import {data} from './data.js';
+
+// Retrieve args
+const args = minimist(process.argv.slice(2));
+
+// create an object using the keys we just determined
+const twitterAPI = new Twitter({
+    consumer_key: process.env.CONSUMER_TOKEN || args.consumer_key,
+    consumer_secret: process.env.CONSUMER_SECRET || args.consumer_secret,
+    access_token_key: process.env.ACCESS_TOKEN_KEY || args.access_token_key,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET || args.access_token_secret
+});
+
+console.log(`Logged in`);
+init();
+
+function tweet(text) {
+    console.info(text);
+    if (!args.test) { // TWEET
+        twitterAPI.post('statuses/update', {
+                status: text.substring(0, 280)
+            },
+            error => console.error('Error: ', error)
+        );
+    }
+}
+
+
+function init() {
+    // http://www.meteofrance.com/mf3-rpc-portlet/rest/carte/france/REG_FRANCE/REGIN05?echeance=201807260000&nightMode=true
+    request('http://www.meteofrance.com/mf3-rpc-portlet/rest/carte/france/REG_FRANCE/REGIN05?echeance=201807260000', (error, response, body) => {
+        console.log('body:', body.previsionLieux);
+    });
+
+    let weather = ['☀'];
+
+    let forecastTime = moment().format('YYYYMMDDhh00');
+
+    let map = `
+\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0${weather[0]}${weather[0]}${weather[0]}
+${weather[0]}${weather[0]}${weather[0]}${weather[0]}${weather[0]}${weather[0]}\xa0\xa0\xa0\xa0${weather[0]}${weather[0]}
+\xa0\xa0\xa0\xa0\xa0${weather[0]}${weather[0]}${weather[0]}${weather[0]}${weather[0]}${weather[0]}${weather[0]}${weather[0]}
+${weather[0]}${weather[0]}${weather[0]}${weather[0]}${weather[0]}${weather[0]}${weather[0]}${weather[0]}${weather[0]}
+\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0${weather[0]}${weather[0]}${weather[0]}${weather[0]}${weather[0]}${weather[0]}${weather[0]}${weather[0]}
+\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0${weather[0]}${weather[0]}${weather[0]}${weather[0]}${weather[0]}
+\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0${weather[0]}${weather[0]}${weather[0]}
+    `;
+
+    tweet(map);
+}
+
---
+++ data.js
@@ -0,0 +1,17 @@
+const data = {
+    WEATHER: {
+        "N_W1_0-N_3": "☁",     // Cloud
+        "N_W1_1-N_0": "🌫",     // Fog
+        "J_W1_24-N_2": "🌩",    // Lightning
+        "J_W1_32-N_2": "⛈",    // Lightning with rain
+        "N_W1_0-N_0": "🌙",     // Clear night
+        "J_W1_9-N_3": "🌧",     // Rain
+        snow: "🌨",
+        "J_W1_0-N_0": "☀",     // Sun
+        "N_W1_0-N_5": "🌤",     // Sun with small cloud
+        "J_W1_0-N_1": "🌥",     // Sun with cloud
+        "J_W1_18-N_1": "🌦",    // Sun with cloud and rain
+    }
+};
+
+export {data};
