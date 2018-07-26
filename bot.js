e1766c13414c2a95e2db34a9e4a5a71c16bd0368
feoche <francois.eoche@corp.ovh.com>
2018-07-26 16:00:47 +0200

    UP

--- bot.js
+++ bot.js
@@ -9,60 +9,52 @@ const args = minimist(process.argv.slice(2));
 
 // create an object using the keys we just determined
 const twitterAPI = new Twitter({
-    consumer_key: process.env.CONSUMER_TOKEN || args.consumer_key,
-    consumer_secret: process.env.CONSUMER_SECRET || args.consumer_secret,
-    access_token_key: process.env.ACCESS_TOKEN_KEY || args.access_token_key,
-    access_token_secret: process.env.ACCESS_TOKEN_SECRET || args.access_token_secret
+  consumer_key: process.env.CONSUMER_TOKEN || args.consumer_key,
+  consumer_secret: process.env.CONSUMER_SECRET || args.consumer_secret,
+  access_token_key: process.env.ACCESS_TOKEN_KEY || args.access_token_key,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET || args.access_token_secret
 });
 
 console.log(`Logged in`);
 init();
 
 function tweet(text) {
-    console.info(text);
-    if (!args.test) { // TWEET
-        twitterAPI.post('statuses/update', {
-                status: text.substring(0, 280)
-            },
-            error => console.error('Error: ', error)
-        );
-    }
+  console.info(text);
+  if (!args.test) { // TWEET
+    twitterAPI.post('statuses/update', {
+        status: text.substring(0, 280)
+      },
+      error => console.error('Error: ', error)
+    );
+  }
 }
 
 
 function init() {
-    // http://www.meteofrance.com/mf3-rpc-portlet/rest/carte/france/REG_FRANCE/REGIN05?echeance=201807260000&nightMode=true
-    request('http://www.meteofrance.com/mf3-rpc-portlet/rest/carte/france/REG_FRANCE/REGIN05?echeance=201807260000', (error, response, body) => {
-        if(error) {
-            console.error('Error: ', error);
+  // http://www.meteofrance.com/mf3-rpc-portlet/rest/carte/france/REG_FRANCE/REGIN05?echeance=201807260000&nightMode=true
+  const url = 'http://www.meteofrance.com/mf3-rpc-portlet/rest/carte/france/REG_FRANCE/REGIN05?echeance=' + moment().add(3, 'hours').format('YYYYMMDDkk00');
+  console.info("url : ", url);
+  request(url, (error, response, body) => {
+    if (error) {
+      console.error('Error: ', error);
+    }
+    const items = JSON.parse(body).previsionLieux
+      .map(a => {
+        return {
+          "x": a.lieu.positionAffichageCarteX,
+          "y": a.lieu.positionAffichageCarteY,
+          "pictoTemps": a.prevision.pictoTemps,
+          "temperature": a.prevision.temperature,
+          "pictoVent": a.prevision.pictoVent
         }
-        console.log('body:', body.previsionLieux);
-        let items = response.previsionLieux.sort((a,b) => {
-            const ay = a.lieu.positionAffichageCarteY,
-                by = b.lieu.a.lieu.positionAffichageCarteY,
-                ax = a.lieu.positionAffichageCarteX,
-                bx = b.lieu.a.lieu.positionAffichageCarteX;
-            if(ay < by && ax < bx) {
-                return -1;
-            }
-            return 0;
-        });
-    });
+      })
+      .filter(a => a.x > data.BOUNDS.x.min && a.x < data.BOUNDS.x.max && a.y > data.BOUNDS.y.min && a.y < data.BOUNDS.y.max)
+      .sort((a, b) => (a.y < (b.y + 50) && a.x < b.x) ? -1 : 1)
+    ;
 
-    let weather = ['☀'];
+    console.info("items : ", items);
+  });
 
-    let forecastTime = moment().format('YYYYMMDDhh00');
-
-    let map = `
-\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0${weather[0]}${weather[0]}${weather[0]}
-${weather[0]}${weather[0]}${weather[0]}${weather[0]}${weather[0]}${weather[0]}\xa0\xa0\xa0\xa0${weather[0]}${weather[0]}
-\xa0\xa0\xa0\xa0\xa0${weather[0]}${weather[0]}${weather[0]}${weather[0]}${weather[0]}${weather[0]}${weather[0]}${weather[0]}
-${weather[0]}${weather[0]}${weather[0]}${weather[0]}${weather[0]}${weather[0]}${weather[0]}${weather[0]}${weather[0]}
-\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0${weather[0]}${weather[0]}${weather[0]}${weather[0]}${weather[0]}${weather[0]}${weather[0]}${weather[0]}
-\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0${weather[0]}${weather[0]}${weather[0]}${weather[0]}${weather[0]}
-\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0${weather[0]}${weather[0]}${weather[0]}
-    `;
-
-    tweet(map);
+  tweet(data.MAP.replace(/#/g, data.WEATHER["J_W1_0-N_0"]));
 }
 
--- data.js
+++ data.js
@@ -1,71 +1,78 @@
 const data = {
-    WEATHER: {
-        "N_W1_0-N_3": "☁",     // Cloud
-        "N_W1_1-N_0": "🌫",     // Fog
-        "J_W1_24-N_2": "🌩",    // Lightning
-        "J_W1_32-N_2": "⛈",    // Lightning with rain
-        "N_W1_0-N_0": "🌙",     // Clear night
-        "J_W1_9-N_3": "🌧",     // Rain
-        snow: "🌨",
-        "J_W1_0-N_0": "☀",     // Sun
-        "N_W1_0-N_5": "🌤",     // Sun with small cloud
-        "J_W1_0-N_1": "🌥",     // Sun with cloud
-        "J_W1_18-N_1": "🌦",    // Sun with cloud and rain
-    },
-    DATACITIES: [
-        // 1st row
-        "saint-pol-de-leon",
-        "lannion",
-        "lannion",
-        // 2nd row - 1st part
-        "brest",
-        "saint-pol-de-leon",
-        "saint-hernin",
-        "saint-hernin",
-        "saint-brieuc",
-        "saint-brieuc",
-        // 2nd row - 2nd part
-        "saint-malo",
-        "saint-malo",
-        // 3rd row
-        "brest",
-        "saint-hernin",
-        "saint-hernin",
-        "saint-brieuc",
-        "saint-brieuc",
-        "saint-malo",
-        "rennes",
-        "fougeres",
-        // 4th row
-        "ile-de-sein",
-        "ile-de-sein",
-        "quimper",
-        "lorient",
-        "pontivy",
-        "pontivy",
-        "taupont",
-        "rennes",
-        "fougeres",
-        // 5th row
-        "quimper",
-        "quimper",
-        "lorient",
-        "lorient",
-        "taupont",
-        "rennes",
-        "rennes",
-        "saint-saturnin-du-limet",
-        // 6th row
-        "lorient",
-        "vannes",
-        "vannes",
-        "rennes",
-        "saint-saturnin-du-limet",
-        // 7th row
-        "vannes",
-        "redon",
-        "redon"
-    ]
+  WEATHER: {
+    "N_W1_0-N_3": "☁",     // Cloud
+    "N_W1_1-N_0": "🌫",     // Fog
+    "J_W1_24-N_2": "🌩",    // Lightning
+    "J_W1_32-N_2": "⛈",    // Lightning with rain
+    "N_W1_0-N_0": "🌙",     // Clear night
+    "J_W1_9-N_3": "🌧",     // Rain
+    snow: "🌨",
+    "J_W1_0-N_0": "☀",     // Sun
+    "N_W1_0-N_5": "🌤",     // Sun with small cloud
+    "J_W1_0-N_1": "🌥",     // Sun with cloud
+    "J_W1_18-N_1": "🌦",    // Sun with cloud and rain
+  },
+  MAP: `\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0###
+######\xa0\xa0\xa0\xa0##
+\xa0\xa0\xa0\xa0\xa0########
+#########
+\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0########
+\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0#####
+\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0###`,
+  DATACITIES: [
+    // 1st row
+    "saint-pol-de-leon",
+    "lannion",
+    "lannion",
+    // 2nd row - 1st part
+    "brest",
+    "saint-pol-de-leon",
+    "saint-hernin",
+    "saint-hernin",
+    "saint-brieuc",
+    "saint-brieuc",
+    // 2nd row - 2nd part
+    "saint-malo",
+    "saint-malo",
+    // 3rd row
+    "brest",
+    "saint-hernin",
+    "saint-hernin",
+    "saint-brieuc",
+    "saint-brieuc",
+    "saint-malo",
+    "rennes",
+    "fougeres",
+    // 4th row
+    "ile-de-sein",
+    "ile-de-sein",
+    "quimper",
+    "lorient",
+    "pontivy",
+    "pontivy",
+    "taupont",
+    "rennes",
+    "fougeres",
+    // 5th row
+    "quimper",
+    "quimper",
+    "lorient",
+    "lorient",
+    "taupont",
+    "rennes",
+    "rennes",
+    "saint-saturnin-du-limet",
+    // 6th row
+    "lorient",
+    "vannes",
+    "vannes",
+    "rennes",
+    "saint-saturnin-du-limet",
+    // 7th row
+    "vannes",
+    "redon",
+    "redon"
+  ]
 };
 
 export {data};
