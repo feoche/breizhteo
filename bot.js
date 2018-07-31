b0f5610dd9ba6a27e74399c62164b54bc497ef23
feoche <francois.eoche@corp.ovh.com>
2018-07-31 15:31:11 +0200

    Wind + temperature

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 import Twitter from 'twitter';
 import minimist from 'minimist';
+import cp from 'child_process';
 import request from 'request';
 import moment from 'moment';
 import {data} from './data.js';
@@ -7,6 +8,7 @@ import {data} from './data.js';
 // Retrieve args
 const args = minimist(process.argv.slice(2));
 let dataItems = [];
+let copy = cp.spawn('pbcopy');
 
 // create an object using the keys we just determined
 const twitterAPI = new Twitter({
@@ -17,18 +19,23 @@ const twitterAPI = new Twitter({
 });
 
 init();
+
 function init() {
   console.log(`Started`);
   queryWeather().then(res => {
     console.info('prevision : ', data);
     tweet(`Prévisions pour ${res}:
     ${textToWeather('pictoTemps')}
+    ${textToWeather('temperature')}
+    ${textToWeather('pictoVent')}
     `);
   });
 }
 
 function tweet(text) {
   console.info(text);
+  copy.stdin.write(text);
+  copy.stdin.end();
   if (!args.test) { // TWEET
     twitterAPI.post('statuses/update', {
         status: text.substring(0, 280)
@@ -40,7 +47,7 @@ function tweet(text) {
 
 function queryWeather() {
   // http://www.meteofrance.com/mf3-rpc-portlet/rest/carte/france/REG_FRANCE/REGIN05?echeance=201807260000&nightMode=true
-  const url = `http://www.meteofrance.com/mf3-rpc-portlet/rest/carte/france/REG_FRANCE/REGIN05?echeance=${moment().format('YYYYMMDD') + ('00' + (data.HOURS.filter(a => a.time>=moment().get('hours'))[0] || data.HOURS[0]).time).substr(-2,2)}00`;
+  const url = `http://www.meteofrance.com/mf3-rpc-portlet/rest/carte/france/REG_FRANCE/REGIN05?echeance=${moment().format('YYYYMMDD') + ('00' + (data.HOURS.filter(a => a.time >= moment().get('hours'))[0] || data.HOURS[0]).time).substr(-2, 2)}00`;
   console.info('url : ', url);
   return new Promise((resolve, reject) => {
     request(url, (error, response, body) => {
@@ -55,32 +62,55 @@ function queryWeather() {
           'y': a.lieu.positionAffichageCarteY,
           'pictoTemps': a.prevision.pictoTemps,
           'temperature': a.prevision.temperature,
-          'pictoVent': a.prevision.pictoVent
+          'pictoVent': a.prevision.pictoVent,
+          'forceVent': a.prevision.forceDuVentMoyenne
         }))
         .filter(a => data.DATACITIES.includes(a.slug) && a.temperature)
       ;
 
       // console.info('dataItems : ', dataItems);
-      resolve((data.HOURS.filter(a => a.time>=moment().get('hours'))[0] || data.HOURS[0]).label);
+      resolve((data.HOURS.filter(a => a.time >= moment().get('hours'))[0] || data.HOURS[0]).label);
     });
   });
 }
 
 function weather(index) {
-  console.info("index : ", index);
   console.info('dataItems.filter(a,i => i === dataItems[index]) : ', dataItems.filter(a => a.slug === data.DATACITIES[index]));
   return dataItems.filter(a => a.slug === data.DATACITIES[index])[0];
 }
 
 function textToWeather(type) {
-  let index = 0;
+  let index = 0, previousSpace = false;
   return data.MAP.split('').map(a => {
-    if(a === '#') {
-      const w = data.WEATHER.filter(a => a.codes.includes(weather(index)[type]))[0].emoji;
+    if (a === '#') {
+      let w, wcity = weather(index)[type];
+      switch (type) {
+        case 'temperature' :
+          w = '';
+          if (previousSpace) {
+            previousSpace = false;
+          }
+          else {
+            w += '\u2003';
+          }
+          w += ('' + wcity).replace(/\w/g, a => data.TEMPERATURES[a]);
+          console.info("w : ", w);
+          break;
+        case 'pictoVent' :
+          const wforce = weather(index)['forceVent'].replace(/[\s<]/g, '').padStart(2, "0");
+          const emoji = data.WINDS.filter(a => a.codes.includes(wcity))[0].emojis;
+          w = emoji[Math.min(emoji.length-1, Math.max(0, Math.floor(wforce[0]) - 1))];
+          break;
+        case 'pictoTemps' :
+        default:
+          w = data.WEATHER.filter(a => a.codes.includes(wcity))[0].emojis[0];
+          break;
+      }
       index++;
       return w;
     }
-    else {
+    else { // the char is a space char
+      previousSpace = true;
       return a;
     }
   }).join('');
--- data.js
+++ data.js
@@ -1,25 +1,56 @@
 const data = {
   WEATHER: [
-    {codes: ["J_W1_0-N_3", "J_W2_3", "N_W1_0-N_3", "N_W2_3"], emoji: "☁"},      // Cloud
-    {codes: ["J_W1_1-N", "J_W1_1-N_0", "J_W1_1-N_1", "J_W1_1-N_2", "J_W1_1-N_3", "J_W1_1-N_4", "J_W1_1-N_5", "J_W1_1-N_6", "J_W1_1-N_7", "J_W1_1-N_8", "J_W1_1-N_9", "J_W1_2-N", "J_W1_2-N_0", "J_W1_2-N_1", "J_W1_2-N_2", "J_W1_2-N_3", "J_W1_2-N_4", "J_W1_2-N_5", "J_W1_2-N_6", "J_W1_2-N_7", "J_W1_2-N_8", "J_W1_2-N_9", "J_W1_33-N", "J_W1_33-N_0", "J_W1_33-N_1", "J_W1_33-N_2", "J_W1_33-N_3", "J_W1_33-N_4", "J_W1_33-N_5", "J_W1_33-N_6", "J_W1_33-N_7", "J_W1_33-N_8", "J_W1_33-N_9", "N_W1_1-N", "N_W1_1-N_0", "N_W1_1-N_1", "N_W1_1-N_2", "N_W1_1-N_3", "N_W1_1-N_4", "N_W1_1-N_5", "N_W1_1-N_6", "N_W1_1-N_7", "N_W1_1-N_8", "N_W1_1-N_9", "N_W1_2-N", "N_W1_2-N_0", "N_W1_2-N_1", "N_W1_2-N_2", "N_W1_2-N_3", "N_W1_2-N_4", "N_W1_2-N_5", "N_W1_2-N_6", "N_W1_2-N_7", "N_W1_2-N_8", "N_W1_2-N_9", "N_W1_33-N", "N_W1_33-N_0", "N_W1_33-N_1", "N_W1_33-N_2", "N_W1_33-N_3", "N_W1_33-N_4", "N_W1_33-N_5", "N_W1_33-N_6", "N_W1_33-N_7", "N_W1_33-N_8", "N_W1_33-N_9", ], emoji: "🌫"},     // Fog
+    {codes: ["J_W1_0-N_3", "J_W2_3", "N_W1_0-N_3", "N_W2_3"], emojis: ["☁"]},      // Cloud
     {
-      codes: ["J_W1_24-N", "J_W1_24-N_0", "J_W1_24-N_1", "J_W1_24-N_2", "J_W1_24-N_3", "J_W1_24-N_4", "J_W1_24-N_5", "J_W1_24-N_6", "J_W1_24-N_7", "J_W1_24-N_8", "J_W1_24-N_9", "J_W1_26-N", "J_W1_26-N_0", "J_W1_26-N_1", "J_W1_26-N_2", "J_W1_26-N_3", "J_W1_26-N_4", "J_W1_26-N_5", "J_W1_26-N_6", "J_W1_26-N_7", "J_W1_26-N_8", "J_W1_26-N_9", "J_W1_31-N", "J_W1_31-N_0", "J_W1_31-N_1", "J_W1_31-N_2", "J_W1_31-N_3", "J_W1_31-N_4", "J_W1_31-N_5", "J_W1_31-N_6", "J_W1_31-N_7", "J_W1_31-N_8", "J_W1_31-N_9", "J_W2_18", "N_W1_24-N", "N_W1_24-N_0", "N_W1_24-N_1", "N_W1_24-N_2", "N_W1_24-N_3", "N_W1_24-N_4", "N_W1_24-N_5", "N_W1_24-N_6", "N_W1_24-N_7", "N_W1_24-N_8", "N_W1_24-N_9", "N_W1_26-N", "N_W1_26-N_0", "N_W1_26-N_1", "N_W1_26-N_2", "N_W1_26-N_3", "N_W1_26-N_4", "N_W1_26-N_5", "N_W1_26-N_6", "N_W1_26-N_7", "N_W1_26-N_8", "N_W1_26-N_9", "N_W1_31-N", "N_W1_31-N_0", "N_W1_31-N_1", "N_W1_31-N_2", "N_W1_31-N_3", "N_W1_31-N_4", "N_W1_31-N_5", "N_W1_31-N_6", "N_W1_31-N_7", "N_W1_31-N_8", "N_W1_31-N_9", "N_W2_18",],
-      emoji: "🌩"
+      codes: ["J_W1_1-N", "J_W1_1-N_0", "J_W1_1-N_1", "J_W1_1-N_2", "J_W1_1-N_3", "J_W1_1-N_4", "J_W1_1-N_5", "J_W1_1-N_6", "J_W1_1-N_7", "J_W1_1-N_8", "J_W1_1-N_9", "J_W1_2-N", "J_W1_2-N_0", "J_W1_2-N_1", "J_W1_2-N_2", "J_W1_2-N_3", "J_W1_2-N_4", "J_W1_2-N_5", "J_W1_2-N_6", "J_W1_2-N_7", "J_W1_2-N_8", "J_W1_2-N_9", "J_W1_33-N", "J_W1_33-N_0", "J_W1_33-N_1", "J_W1_33-N_2", "J_W1_33-N_3", "J_W1_33-N_4", "J_W1_33-N_5", "J_W1_33-N_6", "J_W1_33-N_7", "J_W1_33-N_8", "J_W1_33-N_9", "N_W1_1-N", "N_W1_1-N_0", "N_W1_1-N_1", "N_W1_1-N_2", "N_W1_1-N_3", "N_W1_1-N_4", "N_W1_1-N_5", "N_W1_1-N_6", "N_W1_1-N_7", "N_W1_1-N_8", "N_W1_1-N_9", "N_W1_2-N", "N_W1_2-N_0", "N_W1_2-N_1", "N_W1_2-N_2", "N_W1_2-N_3", "N_W1_2-N_4", "N_W1_2-N_5", "N_W1_2-N_6", "N_W1_2-N_7", "N_W1_2-N_8", "N_W1_2-N_9", "N_W1_33-N", "N_W1_33-N_0", "N_W1_33-N_1", "N_W1_33-N_2", "N_W1_33-N_3", "N_W1_33-N_4", "N_W1_33-N_5", "N_W1_33-N_6", "N_W1_33-N_7", "N_W1_33-N_8", "N_W1_33-N_9",],
+      emojis: ["🌫"]
+    },     // Fog
+    {
+      codes: ["J_W1_24-N", "J_W1_24-N_0", "J_W1_24-N_1", "J_W1_24-N_2", "J_W1_24-N_3", "J_W1_24-N_4", "J_W1_24-N_5", "J_W1_24-N_6", "J_W1_24-N_7", "J_W1_24-N_8", "J_W1_24-N_9", "J_W1_26-N", "J_W1_26-N_0", "J_W1_26-N_1", "J_W1_26-N_2", "J_W1_26-N_3", "J_W1_26-N_4", "J_W1_26-N_5", "J_W1_26-N_6", "J_W1_26-N_7", "J_W1_26-N_8", "J_W1_26-N_9", "J_W1_31-N", "J_W1_31-N_0", "J_W1_31-N_1", "J_W1_31-N_2", "J_W1_31-N_3", "J_W1_31-N_4", "J_W1_31-N_5", "J_W1_31-N_6", "J_W1_31-N_7", "J_W1_31-N_8", "J_W1_31-N_9", "J_W2_18", "N_W1_24-N", "N_W1_24-N_0", "N_W1_24-N_1", "N_W1_24-N_2", "N_W1_24-N_3", "N_W1_24-N_4", "N_W1_24-N_5", "N_W1_24-N_6", "N_W1_24-N_7", "N_W1_24-N_8", "N_W1_24-N_9", "N_W1_26-N", "N_W1_26-N_0", "N_W1_26-N_1", "N_W1_26-N_2", "N_W1_26-N_3", "N_W1_26-N_4", "N_W1_26-N_5", "N_W1_26-N_6", "N_W1_26-N_7", "N_W1_26-N_8", "N_W1_26-N_9", "N_W1_31-N", "N_W1_31-N_0", "N_W1_31-N_1", "N_W1_31-N_2", "N_W1_31-N_3", "N_W1_31-N_4", "N_W1_31-N_5", "N_W1_31-N_6", "N_W1_31-N_7", "N_W1_31-N_8", "N_W1_31-N_9", "N_W2_18"],
+      emojis: ["🌩"]
     },    // Lightning
-    {codes: ["J_W1_32", "J_W2_16", "N_W1_32", "N_W2_16"], emoji: "⛈"},    // Lightning with rain
-    {codes: ["J_W1_9-N_3", "J_W1_18-N_3", "J_W1_30-N_3", "N_W1_9-N_3", "N_W1_18-N_3", "N_W1_30-N_3"], emoji: "🌧"},     // Rain
-    {codes: ["snow"], emoji: "🌨"},
-    {codes: ["J_W1_0-N_0", "J_W1_0-N_7", "J_W2_1", "N_W1_0-N_0", "N_W1_0-N_7", "N_W2_1"], emoji: "☀"},      // Sun
-    {codes: ["J_W1_0-N_5", "N_W1_0-N_5"], emoji: "🌤"},     // Sun with small cloud
+    {codes: ["J_W1_32", "J_W2_16", "N_W1_32", "N_W2_16"], emojis: ["⛈"]},    // Lightning with rain
+    {codes: ["J_W1_9-N_3", "J_W1_18-N_3", "J_W1_30-N_3", "N_W1_9-N_3", "N_W1_18-N_3", "N_W1_30-N_3"], emojis: ["🌧"]},     // Rain
+    {codes: ["snow"], emojis: ["🌨"]},
+    {codes: ["J_W1_0-N_0", "J_W1_0-N_7", "J_W2_1", "N_W1_0-N_0", "N_W1_0-N_7", "N_W2_1"], emojis: ["☀"]},      // Sun
+    {codes: ["J_W1_0-N_5", "N_W1_0-N_5"], emojis: ["🌤"]},     // Sun with small cloud
     {
       codes: ["J_W1_0-N_1", "J_W1_0-N_2", "J_W1_0-N_4", "J_W1_0-N_6", "J_W2_2", "N_W1_0-N_1", "N_W1_0-N_2", "N_W1_0-N_4", "N_W1_0-N_6", "N_W2_2"],
-      emoji: "🌥"
+      emojis: ["🌥"]
     },     // Sun with cloud
     {
       codes: ["J_W1_9-N", "J_W1_9-N_0", "J_W1_9-N_1", "J_W1_9-N_2", "J_W1_9-N_3", "J_W1_9-N_4", "J_W1_9-N_5", "J_W1_9-N_6", "J_W1_9-N_7", "J_W1_9-N_8", "J_W1_9-N_9", "J_W1_18-N", "J_W1_18-N_0", "J_W1_18-N_1", "J_W1_18-N_2", "J_W1_18-N_3", "J_W1_18-N_4", "J_W1_18-N_5", "J_W1_18-N_6", "J_W1_18-N_7", "J_W1_18-N_8", "J_W1_18-N_9", "J_W1_30-N", "J_W1_30-N_0", "J_W1_30-N_1", "J_W1_30-N_2", "J_W1_30-N_3", "J_W1_30-N_4", "J_W1_30-N_5", "J_W1_30-N_6", "J_W1_30-N_7", "J_W1_30-N_8", "J_W1_30-N_9", "J_W2_6", "J_W2_12", "N_W1_9-N", "N_W1_9-N_0", "N_W1_9-N_1", "N_W1_9-N_2", "N_W1_9-N_3", "N_W1_9-N_4", "N_W1_9-N_5", "N_W1_9-N_6", "N_W1_9-N_7", "N_W1_9-N_8", "N_W1_9-N_9", "N_W1_18-N", "N_W1_18-N_0", "N_W1_18-N_1", "N_W1_18-N_2", "N_W1_18-N_3", "N_W1_18-N_4", "N_W1_18-N_5", "N_W1_18-N_6", "N_W1_18-N_7", "N_W1_18-N_8", "N_W1_18-N_9", "N_W1_30-N", "N_W1_30-N_0", "N_W1_30-N_1", "N_W1_30-N_2", "N_W1_30-N_3", "N_W1_30-N_4", "N_W1_30-N_5", "N_W1_30-N_6", "N_W1_30-N_7", "N_W1_30-N_8", "N_W1_30-N_9", "N_W2_6", "N_W2_12"],
-      emoji: "🌦"
+      emojis: ["🌦"]
     },    // Sun with cloud and rain
   ],
+  WINDS: [{
+    codes: ["V_S", "V_SSO", "V_SSE"],
+    emojis: ["🔼", "⬆", "⏫", "🌪️"]
+  }, {
+    codes: ["V_SO"],
+    emojis: ["↗"]
+  }, {
+    codes: ["V_O", "V_OSO", "V_ONO"],
+    emojis: ["▶️", "➡", "⏩", "🌪️"]
+  }, {
+    codes: ["V_NO"],
+    emojis: ["↘"]
+  }, {
+    codes: ["V_N", "V_NNO", "V_NNE"],
+    emojis: ["🔽", "⬇", "⏬", "🌪️"]
+  }, {
+    codes: ["V_NE"],
+    emojis: ["↙"]
+  }, {
+    codes: ["V_E", "V_ENE", "V_ESE"],
+    emojis: ["◀", "⬅", "⏪", "🌪️"]
+  }, {
+    codes: ["V_SE"],
+    emojis: ["↖"]
+  }, {
+    codes: ["V_V"],
+    emojis: ["🔄"]
+  }],
   HOURS: [
     {time: 0, label: "23h -> 2h"},
     {time: 3, label: "2h -> 5h"},
@@ -30,30 +61,31 @@ const data = {
     {time: 18, label: "17h -> 20h"},
     {time: 21, label: "20h -> 23h"}
   ],
-  MAP: `\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0###
-######\xa0\xa0\xa0\xa0##
-\xa0\xa0\xa0\xa0\xa0########
-#########
-\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0########
-\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0#####
-\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0###`,
+  MAP: `\u2003\xa0\xa0###
+######\u2003####
+\u2003\xa0##########
+###########
+\u2003\u2003\xa0#########
+\u2003\u2003\u2003\u2003\u2003\xa0\xa0\xa0######
+\u2003\u2003\u2003\u2003\u2003\u2003\u2003\xa0\xa0\xa0###`,
   DATACITIES: [
     "saint-pol-de-leon", "lannion", "lannion",
     // 2nd row - 1st part
     "brest", "saint-pol-de-leon", "saint-hernin", "saint-hernin", "saint-brieuc", "saint-brieuc",
     // 2nd row - 2nd part
-    "saint-malo", "saint-malo",
+    "saint-malo", "saint-malo", "fougeres", "fougeres",
     // 3rd row
-    "brest", "saint-hernin", "saint-hernin", "saint-brieuc", "saint-brieuc", "saint-malo", "rennes", "fougeres",
+    "brest", "saint-hernin", "saint-hernin", "saint-brieuc", "saint-brieuc", "saint-malo", "rennes", "fougeres", "fougeres", "fougeres",
     // 4th row
-    "ile-de-sein", "ile-de-sein", "quimper", "lorient", "pontivy", "pontivy", "taupont", "rennes", "fougeres",
+    "ile-de-sein", "ile-de-sein", "quimper", "lorient", "pontivy", "pontivy", "taupont", "rennes", "rennes", "rennes", "fougeres",
     // 5th row
-    "quimper", "quimper", "lorient", "lorient", "taupont", "rennes", "rennes", "saint-saturnin-du-limet",
+    "quimper", "quimper", "lorient", "lorient", "taupont", "rennes", "rennes", "rennes", "saint-saturnin-du-limet",
     // 6th row
-    "lorient", "vannes", "vannes", "rennes", "saint-saturnin-du-limet",
+    "lorient", "vannes", "vannes", "rennes", "saint-saturnin-du-limet", "saint-saturnin-du-limet",
     // 7th row
     "vannes", "redon", "redon"
-  ]
+  ],
+  TEMPERATURES: "⁰¹²³⁴⁵⁶⁷⁸⁹".split('')
 };
 
 export {data};
